ea5afc798a961800ba48b9d2e3222e811da6bea3
Hudson <hudson.buzby@namely.com>
2019-04-27 11:07:23 -0400

    working user file

--- models/user.py
+++ models/user.py
@@ -4,14 +4,14 @@ class User:
     all = []
 
     def __init__(self,id,playlist_id,refresh_token):
-        self.id = id
-        self.playlist_id = playlist_id
-        self.refresh_token = refresh_token
+        self.id = str(id)
+        self.playlist_id = str(playlist_id)
+        self.refresh_token = str(refresh_token)
 
     def get_conn(self):
         conn = psycopg2.connect(
                     dbname='postgres',
-                    user='postgres',
+                    user='hudsonbuzby',
                     password='postgres',
                     host='localhost',
                     port=5432,
@@ -25,12 +25,19 @@ class User:
             curr.execute(string)
             resp = curr.fetchall()
         return resp
+    def execute_insert(self,string,values):
+        conn = self.get_conn()
+        with conn.cursor() as curr:
+            curr.execute(string,values)
+            resp = curr.fetchone()[0]
+            conn.commit()
+
+        return resp
     # This function will get a sql connection, format a string to insert a user, and then run execute query on that string
     # If this works, you should see a user in our user table in postgres
     def insert_string(self):
-        return """insert into spotify.user (id, playlist_id, refresh_token)
-                VALUES ('{id}', '{playlist_id}', '{refresh_token}')
-        """.format(id=self.id, playlist_id=self.playlist_id, refresh_token=self.refresh_token)
+        return """insert into spotify.user(id, playlist_id, refresh_token)
+                VALUES (%(id)s, %(playlist_id)s, %(refresh_token)s) RETURNING id """
 
     def select_statement(self,id):
         return """ select * from spotify.user where id = '{user_id}'
@@ -38,7 +45,8 @@ class User:
 
     def save_user(self):
         insert_statement = self.insert_string()
-        self.execute_query(insert_statement)
+        values = {"id":self.id,"playlist_id":self.playlist_id,"refresh_token":self.refresh_token}
+        return self.execute_insert(insert_statement,values)
 
 
     def exists(self,user_id):
